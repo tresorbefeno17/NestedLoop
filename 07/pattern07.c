003b6e04ce7099f8b24108ccbd0f9aeda395381f
BEFENO Trésor <tresorbefeno17@gmail.com>
2024-03-21 22:14:31 +0300

    pattern07.c

---
+++ 07/pattern07.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int i, j, k;
+
+	i = 1;
+	k = 1;
+	while(i <= 5)
+	{
+		j = 1;
+		while( j <= 5)
+		{
+			printf("%2d ", k);
+			k += 2;
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+
+	return (0);
+}
